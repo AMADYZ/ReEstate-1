3be840f7f55c64b65647999e9f4cc9142f51634b
Youssef Sherif <youssef.sherif03@gmail.com>
2023-05-22 22:15:33 +0300

    Modification in frontend Scripts

---
+++ public/Script/filter.js
@@ -0,0 +1,6 @@
+function setButtonValue(value) {
+    document.getElementById('selectedButton').value = value;
+  }
+  function setButtonValueB(value) {
+    document.getElementById('selectedButtonB').value = value;
+  }
