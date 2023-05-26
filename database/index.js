6a1de08bb2d5e45c5a8754693e77e1e009b6ed1c
Abdelrahman Emad <abdelrahmansat2003@gmail.com>
2023-05-26 10:25:05 +0300

    some more changes

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const dotenv=require('dotenv');
 dotenv.config();
 const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@database.z3i1hgm.mongodb.net/RealEstate?retryWrites=true&w=majority`;
 mongoose.connect(dbURI)
-.then(()=>console.log("connected mario"))
+.then(()=>console.log("Connected to DB"))
 .catch((err)=>console.log(err));
 
 //'mongodb://127.0.0.1:27017/LoginTest'
