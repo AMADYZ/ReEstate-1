a77db07e4f7421be43d8b3d8d4a7d7d21a35cd69
Youssef Sherif <youssef.sherif03@gmail.com>
2023-06-04 15:58:57 +0300

    add images and css

---
+++ database/schemas/user.js
@@ -0,0 +1,38 @@
+const mongoos=require('mongoose');
+const bcrypt = require('bcrypt');
+const UserSchema= new mongoos.Schema({
+    username:{
+        type:mongoos.SchemaTypes.String,
+        required:true
+    },
+    email:{
+        type:mongoos.SchemaTypes.String,
+        required:true
+    },
+    phone:{
+        type:mongoos.SchemaTypes.String,
+        required:true
+    },
+    pass:{ 
+        type:mongoos.SchemaTypes.String,
+        required:true
+    },
+    Role:
+    {
+        type:mongoos.SchemaTypes.String,
+        required:true
+    }
+    ,Pending:
+    {
+        type:mongoos.SchemaTypes.String,
+        required:true
+    }
+});
+
+module.exports=mongoos.model('users',UserSchema);
+
+
+
+
+
+
