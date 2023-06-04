982259e594ba2828b04c462495480e682482c7db
Youssef Sherif <youssef.sherif03@gmail.com>
2023-06-04 16:24:27 +0300

    adding scripts

---
+++ database/schemas/properties1.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    title: {
+      type: String
+    },
+    text: {
+      type: String
+    }
+});
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    title: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    condition: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    type: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    location: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    description: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    bedrooms: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    bathrooms: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    area: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    locationgps: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    urllocation: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    images: {
+        type: [String],
+        required: true
+    },
+    price: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    adminmessage: {
+        type: mongoose.SchemaTypes.String
+    },
+    comment: [commentSchema]
+});
+
+module.exports = mongoose.model('properties1', UserSchema);
