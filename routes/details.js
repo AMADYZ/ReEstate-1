900ac26535e03a9254ad98140739be76fc91a4c0
Youssef Sherif <youssef.sherif03@gmail.com>
2023-06-04 18:05:34 +0300

    adding details functionality

---
+++ routes/details.js
@@ -0,0 +1,34 @@
+const Router=require('express');
+const router=Router();
+const properties1=require('../database/schemas/properties1');
+const user1=require('../database/schemas/user');
+const session = require('express-session');
+const user = require('../database/schemas/user');
+
+require('../database/');
+router.get('/', async (req, res) => {
+    const id = req.query.id;
+    const search =await properties1.findById(id);
+    let name =search.username;
+    let title_name=search.title;
+    let user1= await user.find().where("username").equals(name);
+    const similar=await properties1.find().where("title").equals(title_name);
+    res.render('details', { search,user1,similar});
+  });
+
+  router.post('/', async(req,res)=>
+  {
+    const { title, text, id } = req.body;
+    const updatedProperty = await properties1.findByIdAndUpdate(
+      id,
+      { $push: { comment: { title: title, text: text } } },
+      { new: true }
+    );
+    if (updatedProperty) {
+      console.log(updatedProperty);
+    } 
+        
+   res.send({updatedProperty:updatedProperty})
+
+  });
+  module.exports = router;
